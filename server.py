b62607b1c8eca8b4923f9fcb7df469ad1862449b
Kevin Deldycke <kevin@deldycke.com>
2011-05-09 13:21:55 +0000

    Import our local copy of the OOOP module.

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 import os
+import sys
 import cherrypy
+
+# Import the local copy of the OOOP module
+current_folder = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(current_folder, 'lib', 'ooop'))
+from ooop import OOOP
+
+
 
 CONF_NAME = 'server.conf'
 
@@ -7,11 +15,14 @@ CONF_NAME = 'server.conf'
 
 class HelloWorld(object):
     def index(self):
-        return "Hello World!"
+        html = "Hello World!"
+        return html
+        
     index.exposed = True
 
 
+
 if __name__ == '__main__':
-    conf_file = os.path.join(os.path.dirname(__file__), CONF_NAME)
+    conf_file = os.path.join(current_folder, CONF_NAME)
     cherrypy.quickstart(HelloWorld(), config=conf_file)
 
