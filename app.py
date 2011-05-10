1c205e30ff9284986a9828b0c7cd96e52a11b8ce
Kevin Deldycke <kevin@deldycke.com>
2011-05-10 15:18:11 +0000

    Add a view for each partner.

--- app.py
+++ app.py
@@ -9,11 +9,21 @@ class app(object):
     @cherrypy.expose
     @cherrypy.tools.mako(filename="index.html")
     def index(self):
-        partner_names = [p.name for p in self.openerp.ResPartner.all()]
-        return {'partner_names': partner_names}
+        partners = [(p.id, p.name) for p in self.openerp.ResPartner.all()]
+        return {'partners': partners}
 
     @cherrypy.expose
     # TODO: do not expose this when in production
     def default(self, *args, **kwargs):
         return "<html><body><ul><li>args: <code>%s</code></li><li>kwargs: <code>%s</code></li></ul></body></html>" % (args, kwargs)
+
+    @cherrypy.expose
+    @cherrypy.tools.mako(filename="view.html")
+    def view(self, partner_id=None):
+        partner_id = int(partner_id)
+        partner = self.openerp.ResPartner.get(partner_id)
+        return { 'name' : partner.name
+               , 'id'   : partner_id
+               , 'email': partner.email
+               }
 
