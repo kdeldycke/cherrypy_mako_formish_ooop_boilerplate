35dd2081b23e9bce38c32184f301ef23bd7b276e
Kevin Deldycke <kevin@deldycke.com>
2011-05-10 13:28:14 +0000

    Add a default catch-all page to debug our requests.

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ class app(object):
         partner_names = [p.name for p in self.openerp.ResPartner.all()]
         return {'partner_names': partner_names}
 
+    @cherrypy.expose
+    def default(self, *args, **kwargs):
+        return "<html><body><ul><li>args: <code>%s</code></li><li>kwargs: <code>%s</code></li></ul></body></html>" % (args, kwargs)
+
